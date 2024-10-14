80dc45b9352568987e6a879478ea3ff1ec19ccc0
Van Loc Doan <vaan.l.doan@gmail.com>
2024-10-14 10:48:29 +0200

    add db, http controller, service and restapi for booking,movies,screening, seat and user

---
+++ src/routes/index.ts
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+import userRoutes from './users.route';
+import seatRoutes from './seats.route';
+import movieRoutes from './movies.route';
+import screeningsRoutes from './screenings.route';
+import bookingsRoutes from './bookings.route';
+
+const router = Router();
+
+router.use('/users', userRoutes);
+router.use('/seats', seatRoutes);
+router.use('/movies', movieRoutes);
+router.use('/screenings', screeningsRoutes);
+router.use('/bookings', bookingsRoutes);
+
+export default router;

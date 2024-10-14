40b3d64616acd3c5b520f077e27733af3853aae5
Van Loc Doan <vaan.l.doan@gmail.com>
2024-10-14 10:48:29 +0200

    add restapi logic

---
+++ src/controllers/bookings.controller.ts
@@ -0,0 +1,50 @@
+import { Request, Response } from 'express';
+import { bookSeat, getAvailableSeats, getBookedSeats } from '../services/bookings.service';
+
+export const allAvailableSeats = async (req: Request, res: Response) => {
+    try {
+        const id = parseInt(req.params.id, 10);
+
+        if (isNaN(id)) {
+            res.status(400).json({ error: 'Invalid movie id' });
+        }
+
+        const availableSeats = await getAvailableSeats(id);
+
+        if (!availableSeats) {
+            res.status(400).json({ message: 'No available seats' });
+        }
+
+        res.json(availableSeats);
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to fetch' });
+    }
+};
+export const getAllBookedSeats = async (req: Request, res: Response) => {
+    try {
+        const id = parseInt(req.params.id, 10);
+
+        if (isNaN(id)) {
+            res.status(400).json({ error: 'Invalid movie id' });
+        }
+
+        const bookedSeats = await getBookedSeats(id);
+        if (!bookedSeats) {
+            res.status(400).json({ message: 'No booked seats' });
+        }
+        res.json(bookedSeats);
+    } catch (error) {
+        res.status(500).json({ error: 'Failed getting booked seats' });
+    }
+};
+
+export const createBooking = async (req: Request, res: Response) => {
+    try {
+        const { seatIds, screeningId, userId } = req.body;
+        const booking = await bookSeat({ seatIds, screeningId, userId });
+        res.json({ success: true, booking });
+    } catch (error) {
+        console.error(error);
+        res.status(400).json({ success: false, error: 'Failed to create booking' });
+    }
+};
---
+++ src/services/movies.service.ts
@@ -0,0 +1,94 @@
+import { db } from '../db/drizzle';
+import { movieActorsTable, movieDirectorsTable, movieGenresTable, moviesTable } from '../db/schema';
+import { eq } from 'drizzle-orm';
+import { moodifyMovieStructure, movieDbCall, Movies } from './helpers';
+
+export const getAllMovies = async () => {
+    try {
+        const movies = await movieDbCall(false);
+        const moviesWithDirectorsActorsGenres = Array.from(moodifyMovieStructure(movies).values());
+        return moviesWithDirectorsActorsGenres;
+    } catch (error) {
+        throw new Error('Error fetching all movies');
+    }
+};
+
+export const getMovieById = async (movieId: number) => {
+    try {
+        const movies = await movieDbCall(true, movieId);
+        const moviesWithDirectorsActorsGenres = Array.from(moodifyMovieStructure(movies).values());
+        return moviesWithDirectorsActorsGenres;
+    } catch (error) {
+        throw new Error('Error getting movie by id');
+    }
+};
+
+export const updateMovie = async (
+    id: number,
+    updates: Partial<typeof moviesTable.$inferInsert>
+) => {
+    try {
+        const result = await db
+            .update(moviesTable)
+            .set(updates)
+            .where(eq(moviesTable.movie_id, id))
+            .returning();
+        return result[0];
+    } catch (error) {
+        throw new Error('Could not update movie');
+    }
+};
+
+export const deleteMovie = async (id: number) => {
+    try {
+        const result = await db.delete(moviesTable).where(eq(moviesTable.movie_id, id)).returning();
+
+        return result[0];
+    } catch (error) {
+        throw new Error('Could not delete movie');
+    }
+};
+
+export const createMovie = async (movieData: Movies) => {
+    return await db.transaction(async (tx) => {
+        const [movie] = await tx
+            .insert(moviesTable)
+            .values({
+                title: movieData.title,
+                description: movieData.description,
+                duration: movieData.duration,
+                trailer_url: movieData.trailer_url,
+                poster_url: movieData.poster_url,
+            })
+            .returning();
+
+        if (movieData.director_ids.length > 0) {
+            await tx.insert(movieDirectorsTable).values(
+                movieData.director_ids.map((director_id) => ({
+                    movie_id: movie.movie_id,
+                    director_id,
+                }))
+            );
+        }
+
+        if (movieData.actor_ids.length > 0) {
+            await tx.insert(movieActorsTable).values(
+                movieData.actor_ids.map((actor_id) => ({
+                    movie_id: movie.movie_id,
+                    actor_id,
+                }))
+            );
+        }
+
+        if (movieData.genre_ids.length > 0) {
+            await tx.insert(movieGenresTable).values(
+                movieData.genre_ids.map((genre_id) => ({
+                    movie_id: movie.movie_id,
+                    genre_id,
+                }))
+            );
+        }
+
+        return movie;
+    });
+};

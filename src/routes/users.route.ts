0438d25fe8900b3b7ffa14a31338bcb8f61b0c54
Van Loc Doan <vaan.l.doan@gmail.com>
2024-10-21 15:01:23 +0200

    add user booking history

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,10 +1,16 @@
 import { Router } from "express";
-import { getUsers, getUser, create } from "../controllers/users.controller";
+import {
+  getUsers,
+  getUser,
+  create,
+  getBookingHistory,
+} from "../controllers/users.controller";
 
 const router = Router();
 
 router.get("/", getUsers);
 router.get("/:id", getUser);
 router.post("/create", create);
+router.get("/:id/bookinghistory", getBookingHistory);
 
 export default router;

3a863fd15c5d430f41298e9306bc7e79f345a5c9
Van Loc Doan <vaan.l.doan@gmail.com>
2024-10-22 10:22:39 +0200

    add seats base on screenings

--- src/routes/seats.route.ts
+++ src/routes/seats.route.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import {
   getSeats,
   getAllAuditoriumSeats,
+  getSeatMap,
 } from "../controllers/seats.controller";
 
 const router = Router();
 
 router.get("/", getSeats);
+router.get("/seatmap/:id", getSeatMap);
 router.get("/:id", getAllAuditoriumSeats);
 // router.post('/book-seat', book);
 
--- src/services/seats.service.ts
+++ src/services/seats.service.ts
@@ -1,6 +1,11 @@
 import { db } from "../db/drizzle";
-import { bookingsTable, seatsTable } from "../db/schema";
-import { eq } from "drizzle-orm";
+import {
+  auditoriumsTable,
+  bookingsTable,
+  screeningsTable,
+  seatsTable,
+} from "../db/schema";
+import { and, eq, sql } from "drizzle-orm";
 
 export const getAllSeats = async () => {
   try {
@@ -24,27 +29,32 @@ export const getAllSeatsByAuditorium = async (auditoriumId: number) => {
   }
 };
 
-// export const bookSeat = async ({
-//     seatId,
-//     screeningId,
-//     userId,
-// }: {
-//     seatId: number;
-//     screeningId: number;
-//     userId: number;
-// }) => {
-//     try {
-//         const [booking] = await db
-//             .insert(bookingsTable)
-//             .values({
-//                 seat_id: seatId,
-//                 screening_id: screeningId,
-//                 user_id: userId,
-//                 total_price: 300,
-//             })
-//             .returning();
-//         return booking;
-//     } catch (error) {
-//         throw new Error('Could not book seat');
-//     }
-// };
+export const seatMap = async (screeningsId: number) => {
+  try {
+    const seatMap = await db
+      .select({
+        seat_id: seatsTable.seat_id,
+        auditorium_id: seatsTable.auditorium_id,
+        seat_number: seatsTable.seat_number,
+        row_number: seatsTable.row_number,
+        status: sql`CASE WHEN ${bookingsTable.seat_id} IS NULL THEN 'available' ELSE 'booked' END`,
+      })
+      .from(seatsTable)
+      .innerJoin(
+        screeningsTable,
+        eq(seatsTable.auditorium_id, screeningsTable.auditorium_id)
+      )
+      .leftJoin(
+        bookingsTable,
+        and(
+          eq(bookingsTable.seat_id, seatsTable.seat_id),
+          eq(bookingsTable.screening_id, screeningsTable.screening_id)
+        )
+      )
+      .where(eq(screeningsTable.screening_id, screeningsId));
+
+    return seatMap;
+  } catch (error) {
+    throw new Error("Could not get seatmap");
+  }
+};

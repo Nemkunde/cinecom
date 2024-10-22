537d9581ccce1936402e338179a9155a4ddbe426
Van Loc Doan <vaan.l.doan@gmail.com>
2024-10-22 11:16:15 +0200

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
-import express from 'express';
-import routes from './routes';
-import authRoutes from './routes/auth.route';
+import express from "express";
+import routes from "./routes";
+import authRoutes from "./routes/auth.route";
+import cors from "cors";
 
 const app = express();
 const port = 3000;
 
+app.use(cors());
+
 app.use(express.json());
-app.use('/api', routes);
-app.use('/auth', authRoutes);
+app.use("/api", routes);
+app.use("/auth", authRoutes);
 
 app.listen(port, () => {
-    console.log(`server is runnning on localhost:${port}`);
+  console.log(`server is runnning on localhost:${port}`);
 });
